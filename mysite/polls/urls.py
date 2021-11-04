84024b55e4d728c1589a74f6658389d0aeff8c39
Hadise87 <h.shirudi@yahoo.com>
2021-11-05 00:36:26 +0900

    vote in questions

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -11,8 +11,4 @@ urlpatterns = [
     path('<int:question_id>/results/' , views.results, name ='results'),
     # vote
     path('<int:question_id>/vote/' , views.vote, name = 'vote'),
-    # added the word 'specifics'   
-    # delete in namespacing??
-    path('specifics/<int:question_id>/', views.detail, name='detail'),
-
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,15 +1,16 @@
 
 from django.http import Http404
-from django.shortcuts import render
-
+from django.shortcuts import get_object_or_404 , render
+from django.shortcuts import HttpResponse
 # Create your views here.
 
 # part3:
-from django.http import HttpResponse
+from django.http import HttpResponse , HttpResponseRedirect
 from django.template import loader
 # The get_object_or_404() function takes a Django model as its first argument and an arbitrary number of keyword arguments, which it passes to the get() function of the model’s manager. It raises Http404 if the object doesn’t exist.
-from django.shortcuts import get_object_or_404, render
-from .models import Question
+from django.shortcuts import get_object_or_404 , render
+from django.urls import reverse
+from .models import Choice, Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -37,7 +38,25 @@ def results(request, question_id):
     response = "You're looking at the results of question %s."
     return HttoResponse(response % question_id)
 
+    #Having two def results or mix these 2 in one?
+    def results(request, question_id):
+        question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+
+
 def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, DoesNotExist):
+        return render(request, 'polls/detail.html' , {
+            'question': question,
+            'error_message' : "you didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
     return HttpPesponse("You're voting on question %s." % question_id)
 
 

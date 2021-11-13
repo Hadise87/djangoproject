a83dbf1fadd88b4a6224df5d174571e71bcbd1ab
Hadise87 <hadiseh_sh@Hadisehs-MacBook-Air.local>
2021-11-13 15:11:22 +0900

    update views def

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -6,9 +6,11 @@ app_name = 'polls'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    
     path('<int:question_id>/' , views.detail , name='detail'),
     # result
     path('<int:question_id>/results/' , views.results, name ='results'),
     # vote
     path('<int:question_id>/vote/' , views.vote, name = 'vote'),
 ]
+
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -12,34 +12,21 @@ from django.shortcuts import get_object_or_404 , render
 from django.urls import reverse
 from .models import Choice, Question
 
+# 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list' : latest_question_list}
     return render(request, 'polls/index.html', context)
-    # output = ', '.join([q.question_text for q in latest_question_list])
-    # return HttpResponse(output)
-    return HttpResponse(template.render(context, request))
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index now.")
+
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html' , {'question' : question})
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    return render(request, 'polls/detail.html' , {'question' : question})
-    return HttpResponse("You're looking at question %s." %question_id)
+
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttoResponse(response % question_id)
-
-    #Having two def results or mix these 2 in one?
-    def results(request, question_id):
-        question = get_object_or_404(Question, pk=question_id)
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'question': question})
 
 
@@ -56,7 +43,7 @@ def vote(request, question_id):
         selected_choice.votes += 1
         selected_choice.save()
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
-    return HttpPesponse("You're voting on question %s." % question_id)
+    # return HttpPesponse("You're voting on question %s." % question_id)
 
 
 

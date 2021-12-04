e415f08fed1529391053c04f391308c2625e8b80
Hadise87 <h.shirudi@gmail.com>
2021-12-04 14:57:07 +0900

    make test

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -5,11 +5,17 @@ from . import views
 app_name = 'polls'
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    
-    path('<int:question_id>/' , views.detail , name='detail'),
+    # path('', views.index, name='index'),  for generic view we changed it to belo form:
+    path('',views.IndexView.as_view(), name='index'),
+   
+    # And also dont need the below form for detail  
+    # path('<int:question_id>/' , views.detail , name='detail'), 
+    # instead:
+    path('<int:pk>/', views.DetailView.as_view(), name ='detail'),
     # result
-    path('<int:question_id>/results/' , views.results, name ='results'),
+    # path('<int:question_id>/results/' , views.results, name ='results'),
+    # changing to:
+   path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     # vote
     path('<int:question_id>/vote/' , views.vote, name = 'vote'),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,34 +1,66 @@
 
-from django.http import Http404
-# from django.shortcuts import HttpResponse
-from django.http import HttpResponse , HttpResponseRedirect
-from django.template import loader
+from django.http import  HttpResponseRedirect
+# from django.template import loader
 # The get_object_or_404() function takes a Django model as its first argument and an arbitrary number of keyword arguments, which it passes to the get() function of the model’s manager. It raises Http404 if the object doesn’t exist.
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.views import generic
+
 from .models import Choice, Question
+from django.utils import timezone
 
-# 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list' : latest_question_list}
-    return render(request, 'polls/index.html', context)
+# As we had in generic definition generic views abstract common patterns to the point where you don’t even need to write Python code to write an app. so we changed add def to class
+
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     context = {'latest_question_list' : latest_question_list}
+#     return render(request, 'polls/index.html', context)
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+#  In question.object .... ye queryset barmigardune ke dakhele an Questions haii hast ke pub_date anha zudtar az timezone alane.
+    def get_queryset(self):
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
+
+#  In question.object .... ye queryset barmigardune ke dakhele an Questions haii hast ke pub_date anha zudtar az timezone alane.
+
+class DetailView(generic.DetailView):
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date_lte=timezone.now())
 
 
-
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html' , {'question' : question})
-
-
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 
+                # we dont need these two def also:
+
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/detail.html' , {'question' : question})
+
+
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/results.html', {'question': question})
+
+                    # This def for vote as before
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
+        # request.POST va request.POST['choice'] hamishe outpiteshun string hast
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
         # Redisplay the question voting form.

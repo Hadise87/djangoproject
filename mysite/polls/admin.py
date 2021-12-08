0f94258587bc166c02851a1b424e84828fa809d8
Hadise87 <h.shirudi@gmail.com>
2021-12-08 23:00:23 +0900

    pesonalize poll's question and answers

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,6 +1,30 @@
 from django.contrib import admin
+from .models import Choice, Question
 
-# Register your models here.
-from .models import Question
 
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,                 {'fields' : ['question_text']}),
+        ('Data informatiomn',  {'fields': ['pub_date'], 'classes':['collapse']}),
+    ]
+    # avakin  element dar har majmue chandtaii fieldset, hamun onvane un fieldset hast.
+
+    inlines = [ChoiceInline]
+    
+    # For good measure, let’s also include the was_published_recently() method
+    list_display = ('question_text', 'pub_date')
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+
+
+admin.site.register(Question, QuestionAdmin)
+        
+
+
+
+
+
--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,6 +1,5 @@
 from django.db import models
-
-# Create your models here.
+from django.contrib import admin
 
 import datetime
 
@@ -17,6 +16,14 @@ class Question(models.Model):
 
     def __str__(self):
         return self.question_text
+    @admin.display(
+        boolean = True,
+        ordering='pub_date',
+        description = 'published recently?',
+    )
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 
